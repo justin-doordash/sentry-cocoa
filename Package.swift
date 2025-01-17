3a04a79e0db3d3b8e7d63c3d868954b4d97b4054
justin-doordash <justin.lennox@doordash.com>
2025-01-17 18:45:22 -0500

    Artifactory Packages

--- Package.swift
+++ Package.swift
@@ -12,12 +12,12 @@ let package = Package(
     targets: [
         .binaryTarget(
                     name: "Sentry",
-                    url: "https://github.com/getsentry/sentry-cocoa/releases/download/8.43.0-beta.1/Sentry.xcframework.zip",
+                    url: "https://ddartifacts.jfrog.io/artifactory/swift-general-local/Sentry/8.43.0/Sentry.xcframework.zip",
                     checksum: "2c046fe71d1f55f3c68b09f44b298df7a5c741415ed2779a317f0a4c3b2f7af7" //Sentry-Static
                 ),
         .binaryTarget(
                     name: "Sentry-Dynamic",
-                    url: "https://github.com/getsentry/sentry-cocoa/releases/download/8.43.0-beta.1/Sentry-Dynamic.xcframework.zip",
+                    url: "https://ddartifacts.jfrog.io/artifactory/swift-general-local/Sentry/8.43.0/Sentry-Dynamic.xcframework.zip",
                     checksum: "ce3ffc5015b279c59c3f6012ce0bab5ffeb34e1db806408258cbfec7ea1d6e8b" //Sentry-Dynamic
                 ),
         .target ( name: "SentrySwiftUI",
