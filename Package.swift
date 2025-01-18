1bd3b338f333a92b69ac10fe1f4e924889efa4b0
justin-doordash <justin.lennox@doordash.com>
2025-01-17 19:08:52 -0500

    Fix checksums

--- Package.swift
+++ Package.swift
@@ -13,12 +13,12 @@ let package = Package(
         .binaryTarget(
                     name: "Sentry",
                     url: "https://ddartifacts.jfrog.io/artifactory/swift-general-local/Sentry/8.43.0/Sentry.xcframework.zip",
-                    checksum: "2c046fe71d1f55f3c68b09f44b298df7a5c741415ed2779a317f0a4c3b2f7af7" //Sentry-Static
+                    checksum: "5e208e121435ae8e037eab1ff3c2f041e7fb4b3e2e6e8d1613837934c3b9f3c5" //Sentry-Static
                 ),
         .binaryTarget(
                     name: "Sentry-Dynamic",
                     url: "https://ddartifacts.jfrog.io/artifactory/swift-general-local/Sentry/8.43.0/Sentry-Dynamic.xcframework.zip",
-                    checksum: "ce3ffc5015b279c59c3f6012ce0bab5ffeb34e1db806408258cbfec7ea1d6e8b" //Sentry-Dynamic
+                    checksum: "9665f4912219bfd7be2954e7d3084cbe2ecfb28d1f1ebd1d1bb6ea735135795d" //Sentry-Dynamic
                 ),
         .target ( name: "SentrySwiftUI",
                   dependencies: ["Sentry", "SentryInternal"],
