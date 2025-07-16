eea895dcf127f434ddacad59b0f4be160ded632e
Philip Sawyer <philip.sawyer@doordash.com>
2025-07-16 15:09:31 -0700

    Redo fix merge issue

--- Package.swift
+++ Package.swift
@@ -1,39 +1,93 @@
+// swift-tools-version:5.3
+#if canImport(Darwin)   
+import Darwin.C
+#elseif canImport(Glibc)
+import Glibc
+#elseif canImport(MSVCRT)
+import MSVCRT
+#endif
+
 import PackageDescription
+
+var products: [Product] = [
+    .library(name: "Sentry", targets: ["Sentry"]),
+    .library(name: "Sentry-Dynamic", targets: ["Sentry-Dynamic"]),
+    .library(name: "SentrySwiftUI", targets: ["Sentry", "SentrySwiftUI"])
+]
+
+var targets: [Target] = [
+    .binaryTarget(
+        name: "Sentry",
+        url: "https://ddartifacts.jfrog.io/artifactory/swift-general-local/Sentry/8.53.2/Sentry.xcframework.zip",
+        checksum: "b4c0b46ea2752d5fbf2b98b2c4bddc71e8803714b1faff0f2e427082474a7d3e" //Sentry-Static
+    ),
+    .binaryTarget(
+        name: "Sentry-Dynamic",
+        url: "https://ddartifacts.jfrog.io/artifactory/swift-general-local/Sentry/8.53.2/Sentry-Dynamic.xcframework.zip",
+        checksum: "d0a293654ab99979c5aae2a4a7ce50a4c40c62e36c98fb3c7b2bc1adcc2621d1" //Sentry-Dynamic
+    ),
+    .target (
+        name: "SentrySwiftUI",
+        dependencies: ["Sentry", "SentryInternal"],
+        path: "Sources/SentrySwiftUI",
+        exclude: ["SentryInternal/", "module.modulemap"],
+        linkerSettings: [
+            .linkedFramework("Sentry")
+        ]),
+    .target(
+        name: "SentryInternal",
+        path: "Sources/SentrySwiftUI",
+        sources: [
+            "SentryInternal/"
+        ],
+        publicHeadersPath: "SentryInternal/")
+]
+
+let env = getenv("EXPERIMENTAL_SPM_BUILDS")
+if let env = env, String(cString: env, encoding: .utf8) == "1" {
+    products.append(.library(name: "SentrySPM", type: .dynamic, targets: ["SentryObjc"]))
+    targets.append(contentsOf: [
+        // At least one source file is required
+        .target(name: "SentryHeaders", path: "Sources/Sentry", sources: ["SentryDsn.m"], publicHeadersPath: "Public"),
+        .target(
+            name: "_SentryPrivate",
+            dependencies: ["SentryHeaders"],
+            path: "Sources/Sentry",
+            sources: ["NSLocale+Sentry.m"],
+            publicHeadersPath: "include",
+            cSettings: [.headerSearchPath("include/HybridPublic")]),
+        .target(
+            name: "SentrySwift",
+            dependencies: ["_SentryPrivate", "SentryHeaders"],
+            path: "Sources/Swift",
+            swiftSettings: [
+                .unsafeFlags(["-enable-library-evolution"]),
+                // This flag is used to make some API breaking changes necessary for the framework to compile with SPM.
+                // We can either make more extensive changes to allow it to be backwards compatible, or release them as part of a V9 release.
+                // For now we use this flag so that CI can compile the SPM version.
+                    .define("SENTRY_SWIFT_PACKAGE")
+            ]),
+        .target(
+            name: "SentryObjc",
+            dependencies: ["SentrySwift"],
+            path: "Sources",
+            exclude: ["Sentry/SentryDsn.m", "Sentry/NSLocale+Sentry.m", "Swift", "SentrySwiftUI", "Resources", "Configuration"],
+            cSettings: [
+                .headerSearchPath("Sentry/include/HybridPublic"),
+                .headerSearchPath("Sentry"),
+                .headerSearchPath("SentryCrash/Recording"),
+                .headerSearchPath("SentryCrash/Recording/Monitors"),
+                .headerSearchPath("SentryCrash/Recording/Tools"),
+                .headerSearchPath("SentryCrash/Installations"),
+                .headerSearchPath("SentryCrash/Reporting/Filters"),
+                .headerSearchPath("SentryCrash/Reporting/Filters/Tools")])
+    ])
+}
 
 let package = Package(
     name: "Sentry",
     platforms: [.iOS(.v11), .macOS(.v10_13), .tvOS(.v11), .watchOS(.v4)],
-    products: [
-        .library(name: "Sentry", targets: ["Sentry"]),
-        .library(name: "Sentry-Dynamic", targets: ["Sentry-Dynamic"]),
-        .library(name: "SentrySwiftUI", targets: ["Sentry", "SentrySwiftUI"])
-    ],
-    targets: [
-        .binaryTarget(
-                    name: "Sentry",
-                    url: "https://ddartifacts.jfrog.io/artifactory/swift-general-local/Sentry/8.46.0/Sentry.xcframework.zip",
-                    checksum: "aaf27ced063c9fcdcddb9ad1e9b63524b21f31165e6e538d35e28c36ef5fca2e" //Sentry-Static
-                ),
-        .binaryTarget(
-                    name: "Sentry-Dynamic",
-                    url: "https://ddartifacts.jfrog.io/artifactory/swift-general-local/Sentry/8.46.0/Sentry-Dynamic.xcframework.zip",
-                    checksum: "60435d0920f9f0986c087f2f64dc6d09ca553f0945326e8609a1aa727f3f33e9" //Sentry-Dynamic
-                ),
-        .target ( name: "SentrySwiftUI",
-                  dependencies: ["Sentry", "SentryInternal"],
-                  path: "Sources/SentrySwiftUI",
-                  exclude: ["SentryInternal/", "module.modulemap"],
-                  linkerSettings: [
-                     .linkedFramework("Sentry")
-                  ]
-                ),
-        .target( name: "SentryInternal",
-                 path: "Sources/SentrySwiftUI",
-                 sources: [
-                    "SentryInternal/"
-                 ],
-                 publicHeadersPath: "SentryInternal/"
-               )
-    ],
+    products: products,
+    targets: targets,
     cxxLanguageStandard: .cxx14
 )
